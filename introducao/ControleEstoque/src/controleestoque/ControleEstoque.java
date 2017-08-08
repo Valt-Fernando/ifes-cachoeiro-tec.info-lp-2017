ed460f316fd6afa5cb07f83cecf43e8e6c1693a1
Alexandre Romanelli <alexromanelli@gmail.com>
2017-08-08 20:14:02 -0300

    Inclusão do projeto ControleEstoque

---
+++ introducao/ControleEstoque/src/controleestoque/ControleEstoque.java
@@ -0,0 +1,19 @@
+/*
+ * Se for usar este código, cite o autor.
+ */
+package controleestoque;
+
+/**
+ *
+ * @author Alexandre Romanelli <alexandre.romanelli@ifes.edu.br>
+ */
+public class ControleEstoque {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ introducao/ControleEstoque/src/controleestoque/entidades/Produto.java
@@ -0,0 +1,46 @@
+/*
+ * Se for usar este código, cite o autor.
+ */
+package controleestoque.entidades;
+
+/**
+ *
+ * @author Alexandre Romanelli <alexandre.romanelli@ifes.edu.br>
+ */
+public class Produto {
+    // atributos:
+    private long codigo;
+    private String nome;
+    private double preco;
+    
+    public Produto(long codigo, String nome, double preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    
+}
